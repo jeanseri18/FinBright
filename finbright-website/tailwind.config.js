1df924b9f07d891ab9898d24bcdd267d8824a063
Moussa Fofana <mfofana@aguimaweb.com>
2025-07-14 20:26:59 +0000

    Préparation de ton projet: final

--- finbright-website/tailwind.config.js
+++ finbright-website/tailwind.config.js
@@ -1,13 +1,13 @@
-export default {
-    content: [
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
-        './storage/framework/views/*.php',
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './resources/views/**/*.blade.php',
+    './resources/js/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
